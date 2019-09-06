64da63b91d4366842aeb5704ec2df43aebcd2a96
回头太难 <lan-cin@qq.com>
2019-09-06 16:41:43 +0800

    add comparison count

--- sort/insertion_sort/insertion_sort.c
+++ sort/insertion_sort/insertion_sort.c
@@ -1,14 +1,20 @@
 #include <stdio.h>
 
 void insertion_sort(int *arr, int size){
+    int compare_time = 0;
     for(int i = 1; i < size; i++){
         if( arr[i - 1] > arr[i] ){
+            compare_time += 1;
             int t = arr[i];
             int j;
-            for(j = i - 1; j >= 0 && arr[j] > t; j--) arr[j + 1] = arr[j];
+            for(j = i - 1; j >= 0 && arr[j] > t; j--) {
+                arr[j + 1] = arr[j];
+                compare_time += 1;
+            }
             arr[j + 1] = t;
         }
     }
+    printf("compare_time: %d\n", compare_time);
 }
 
 void print(int *arr, int size){
@@ -19,8 +25,10 @@ void print(int *arr, int size){
 }
 
 void bin_insertion_sort(int *arr, int size){
-    for(int i = 1; i < size; i++){
+     int compare_time = 0;
+     for(int i = 1; i < size; i++){
         if(arr[i -1] > arr[i]){
+            compare_time += 1;
             int t = arr[i];
             int lo = 0;
 //            print(arr, size);
@@ -32,6 +40,7 @@ void bin_insertion_sort(int *arr, int size){
                 }else{
                     lo = mid + 1;
                 }
+                compare_time += 1;
             }
 //            printf("low: %d, high: %d\n\n", lo, hi);
             int j = i - 1;
@@ -39,14 +48,15 @@ void bin_insertion_sort(int *arr, int size){
             arr[lo] = t;
         }
     }
+    printf("compare_time: %d\n", compare_time);
 }
 
 
 
 void main(){
-    int arr[] = {1,3,4,6,2,3,5,25,32,12,3};
-    bin_insertion_sort(arr, 10);
-    for(int i = 0; i < 10; i++){
+    int arr[] = {1,3,4,6,2,3,5,25,32,12,3,2,3,5,6,7,21,4,3,1,3,4,};
+    bin_insertion_sort(arr, 20);
+    for(int i = 0; i < 20; i++){
         printf("%d, ", arr[i]);
     }
 }
