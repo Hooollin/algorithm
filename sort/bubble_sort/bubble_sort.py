eb5f6f78f3d6030b2a42525f9905e8f9554e3471
回头太难 <lan-cin@qq.com>
2019-09-07 15:06:16 +0800

    add implementation of double direction bubble sort

--- sort/bubble_sort/bubble_sort.py
+++ sort/bubble_sort/bubble_sort.py
@@ -14,3 +14,28 @@ def reverse_arr(arr):
         arr[start], arr[end] = arr[end], arr[start]
         start += 1
         end -= 1
+
+
+def double_direction_bubble_sort(arr):
+    front_index, end_index = 0, len(arr)
+    finished = False
+    while not finished and front_index < end_index:
+        finished = True
+        if (front_index + end_index) % 2 == 0:
+            for i in range(front_index + 1, end_index):
+                if arr[i] < arr[i - 1]:
+                    arr[i], arr[i - 1] = arr[i - 1], arr[i]
+                    finished = False
+            end_index -= 1
+            print(arr)
+        else:
+            for i in range(end_index - 1, front_index, -1):
+                if arr[i] < arr[i - 1]:
+                    arr[i], arr[i - 1] = arr[i - 1], arr[i]
+                    finished = False
+            front_index += 1
+            print(arr)
+    print(arr)
+
+
+double_direction_bubble_sort([12, 3, 5, 2, 34, 2, 5, 6, 1, 3])
